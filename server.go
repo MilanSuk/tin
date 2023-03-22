b3ea418df59b72e3acd24f256fe5143d34dd0787
milansuk <milansuk86@gmail.com>
2023-03-22 18:03:17 +0100

    Net renamed to Server, Add Websocket server

---
+++ server.go
@@ -0,0 +1,144 @@
+/*
+Copyright 2023 Milan Suk
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/websocket"
+)
+
+type Server struct {
+	txnsPool   *Pool
+	blocksPool *Pool
+
+	server         http.Server
+	isServerClosed bool
+
+	//thread OsThread
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func NewNet(ssl_on bool, port int) (*Server, error) {
+	var net Server
+
+	net.txnsPool = NewPool()
+	net.blocksPool = NewPool()
+
+	go net.Loop(ssl_on, port)
+
+	return &net, nil
+}
+
+func (net *Server) Destroy() error {
+	net.isServerClosed = true
+	net.server.Close()
+	//net.thread.Wait()
+	return nil
+}
+
+const MSG_TXN = 0
+const MSG_BLOCK = 1
+
+func (net *Server) Loop(ssl_on bool, port int) error {
+
+	//defer net.thread.End()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Printf("Client accepted %s\n", r.URL.Path)
+
+		if r.URL.Path == "/" {
+			http.ServeFile(w, r, "tin.html")
+			return
+		} else if r.URL.Path == "/data" {
+
+			c, err := upgrader.Upgrade(w, r, nil)
+			if err != nil {
+				log.Println("Error: RunHub() failed: %v", err)
+				return
+			}
+			defer c.Close()
+
+			for {
+
+				mt, message, err := c.ReadMessage()
+
+				if mt == websocket.CloseMessage || websocket.IsCloseError(err) || websocket.IsUnexpectedCloseError(err) {
+					return
+				}
+
+				if err != nil {
+					log.Println("Error: ReadMessage() failed: %v", err)
+					return
+				}
+
+				if mt != websocket.BinaryMessage {
+					log.Println("Error: ReadMessage() is not binary: %v", err)
+					return
+				}
+
+				if len(message) < 2 {
+					log.Printf("Error: ReadMessage() is too small: %v", err)
+					return
+				}
+
+				//var ans []byte
+				if message[0] == MSG_TXN {
+					net.txnsPool.Add(message[1:])
+				} else if message[0] == MSG_BLOCK {
+					net.blocksPool.Add(message[1:])
+				}
+
+				/*err = c.WriteMessage(mt, ans)
+				if err != nil {
+					log.Println("Error: WriteMessage() failed: %w", err)
+					return
+				}*/
+			}
+		}
+	})
+
+	net.server = http.Server{Addr: ":" + strconv.Itoa(port), Handler: mux}
+
+	var err error
+	if ssl_on {
+		err = net.server.ListenAndServeTLS("ssl/cert.pem", "ssl/key.pem")
+	} else {
+		err = net.server.ListenAndServe()
+	}
+
+	//show error only if loop is not closed naturaly
+	if err != nil && !net.isServerClosed {
+		log.Printf("Net.Loop() failed: %v\n", err)
+		return err
+	}
+
+	//for net.thread.Is() {
+	//	time.Sleep(10 * time.Millisecond)
+	//}
+
+	return nil
+}
